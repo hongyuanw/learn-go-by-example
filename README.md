523093f1e4ba4c0f98664486151ba1e12b1bfba6
yuanwhy <henrywangwhy@gmail.com>
2017-06-29 22:47:10 +0800

    finish map

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ https://gobyexample.com/
 - [x] Switch
 - [x] Arrays
 - [x] Slices
-- [ ] Maps
+- [x] Maps
 - [ ] Range
 - [ ] Functions
 - [ ] Multiple Return Values
---
+++ maps.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+
+	m["k1"] = 1
+	m["k2"] = 3
+
+	fmt.Println("map: ", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+
+	fmt.Println("len: ", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map: ", m)
+
+	_, exist := m["k2"]
+	fmt.Println("k2 exist :", exist)
+
+	n := map[string]int{"foo1": 1, "foo2": 2}
+	fmt.Println("n : ", n)
+
+}
