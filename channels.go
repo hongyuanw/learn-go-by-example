5fe476bb0584e90569d8d14768e5da30bde4c1c6
yuanwhy <henrywangwhy@gmail.com>
2017-07-09 14:10:00 +0800

    test blocking

--- channels.go
+++ channels.go
@@ -1,15 +1,19 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 
 	message := make(chan string) // channel
 
 	go func() {
+		time.Sleep(2 * time.Second)
+		fmt.Println("into another goruoutie")
 		message <- "ping"
 	}()
 
+	fmt.Println("waiting ...")
 	msg := <-message
 	fmt.Println(msg)
 
