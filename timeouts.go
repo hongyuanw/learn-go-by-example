87fd549c75a9edd2619b9f0e2a5de858ec0ce306
yuanwhy <henrywangwhy@gmail.com>
2017-07-11 23:20:51 +0800

    time.After return channel

--- timeouts.go
+++ timeouts.go
@@ -14,7 +14,9 @@ func main() {
 	select {
 	case msg := <-c1:
 		fmt.Println("get ", msg)
+
 	case <-time.After(time.Second * 2):
+		// https://golang.org/pkg/time/#After  After return a channel : <-chan Time
 		fmt.Println("timeout..")
 	}
 
