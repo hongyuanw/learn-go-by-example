790ca157998bc6c1364fb8251342f29fd13d682b
Hongyuan Wang <henrywangwhy@gmail.com>
2017-08-14 22:09:23 +0800

    filter collection function

--- collection-functions.go
+++ collection-functions.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strings"
 
 // Index find string in slices
 func Index(s []string, t string) int {
@@ -13,8 +14,29 @@ func Index(s []string, t string) int {
 	return -1
 }
 
+func Include(s []string, t string) bool {
+	return Index(s, t) >= 0
+}
+
+func Filter(s []string, f func(string) bool) []string {
+	tmps := make([]string, 0)
+
+	for _, str := range s {
+		if f(str) {
+			tmps = append(tmps, str)
+		}
+	}
+
+	return tmps
+}
+
 func main() {
 	var strs = []string{"peef", "bee", "ef"}
 
 	fmt.Println(Index(strs, "bee"))
+	fmt.Println(Include(strs, "de"))
+
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "b")
+	}))
 }
